c6e3fc9b7f02a3e21d966537290ba9f84211e9a7
dangjose <dangjos02@gmail.com>
2025-06-17 11:42:02 +1000

    change to getters and setters

--- part 2/hbnb/app/models/user.py
+++ part 2/hbnb/app/models/user.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+import re
 
 class User:
     def __init__(self, first_name, last_name, email, password, is_admin=False):
@@ -11,32 +12,45 @@ class User:
         self.updated_at = self.created_at
 
         # Validated fields
-        self.first_name = self.validate_name(first_name, 'First name')
-        self.last_name = self.validate_name(last_name, 'Last name')
-        self.email = self.validate_email(email)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
         self.is_admin = is_admin
         self.password = password
 
-    def validate_name(self, name, field):
-        if not name or len(name.strip()) == 0 or len(name) > 50:
-            raise ValueError(f"{field} must be between 1 and 50 characters")
-        return name.strip()
+    @property
+    def get_first_name(self):
+        return self.first_name
 
-    def validate_email(self, email):
-        if not email or len(email.strip()) == 0:
-            raise ValueError(f"Email must be between 1 and 254 characters")
-        if '@' in email:
-            return email.strip()
-        else:
-            raise ValueError(f"Email must have @ symbol")
+    @get_first_name.setter
+    def __set_first_name(self, first_name):
+        if not first_name or len(first_name.strip()) == 0 or len(first_name) > 50:
+            raise ValueError("First name must be between 1 and 50 characters")
+        return first_name.strip()
+
+    @property
+    def get_last_name(self):
+        return self.last_name
+
+    @get_last_name.setter
+    def __set_last_name(self, last_name):
+        if not last_name or len(last_name.strip()) == 0 or len(last_name) > 50:
+            raise ValueError("First name must be between 1 and 50 characters")
+        return last_name.strip()
+
+    @property
+    def get_email(self):
+        return self.email
+
+    @get_email.setter
+    def __set_email(self, email):
+        return re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email)
 
     # def create_user(self, first_name, last_name, email, password):
     #     #Save to database ?????
 
     # def validate_userid(self):
         # if self.id already exists, run uuid again
-
-
 
     def update_user(self):
         self.updated_at = datetime.now()
