55c46b7d629a0ee88a41a45eeefc9605e8d144d6
DillonIs <dillonis826@gmail.com>
2025-06-22 12:54:56 +1000

    Added review logic to facade file

--- part 2/hbnb/app/services/facade.py
+++ part 2/hbnb/app/services/facade.py
@@ -1,5 +1,6 @@
 """Placeholder for Services facade.py"""
 from app.persistence.repository import InMemoryRepository
+from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
@@ -17,3 +18,40 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    """Review facade"""
+    def create_review(self, review_data):
+        # Placeholder for logic to create a review, including validation for user_id, place_id, and rating
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review
+        #pass
+
+    def get_review(self, review_id):
+        # Placeholder for logic to retrieve a review by ID
+        return self.review_repo.get(review_id)
+        #pass
+
+    def get_all_reviews(self):
+        # Placeholder for logic to retrieve all reviews
+        return self.review_repo.get_all()
+        #pass
+
+    def get_reviews_by_place(self, place_id):
+        # Placeholder for logic to retrieve all reviews for a specific place
+        """double check review_repo.get_all"""
+        return self.review_repo.get_all(place_id)
+        #pass
+
+    def update_review(self, review_id, review_data):
+        # Placeholder for logic to update a review
+        review_update = self.review_repo.get(review_id)
+        """Placeholder Update review with the new review_data"""
+        return review_update
+        #pass
+
+    def delete_review(self, review_id):
+        # Placeholder for logic to delete a review
+        return self.review_repo.delete(review_id)
+    """Expecting Boolean"""
+        #pass
