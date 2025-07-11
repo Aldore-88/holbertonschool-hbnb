7010bc876b9993c095f25d53372a4b1a9db71bf7
dangjose <dangjos02@gmail.com>
2025-07-11 20:13:10 +1000

    add update method to amenity class

--- part 2/hbnb/app/models/amenity.py
+++ part 2/hbnb/app/models/amenity.py
@@ -9,21 +9,31 @@ class Amenity:
 
         Args:
         """
-        self.amenities_id = str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        self.set_name = name
+        self.name = name
 
     @property
     def name(self):
         return self.__name
 
     @name.setter
-    def set_name(self, name):
+    def name(self, name):
         if not name or len(name.strip()) == 0 or len(name) > 50:
             raise ValueError("First name must be between 1 and 50 characters")
         else:
             self.__name = name.strip()
+
+    def update(self, data):
+        allowed_keys = [
+            'name',
+            ]
+
+        for key, value in data.items():
+            if key in allowed_keys:
+                setattr(self, key, value)
+        return self
 
     def save(self):
         self.updated_at = datetime.now()
