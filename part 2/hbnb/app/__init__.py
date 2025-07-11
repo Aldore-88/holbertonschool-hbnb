fda23ed27b432de8923673811f85b1dec132bdf1
dangjose <dangjos02@gmail.com>
2025-07-11 20:12:23 +1000

    amenities endpoint

--- part 2/hbnb/app/__init__.py
+++ part 2/hbnb/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 
 def create_app():
@@ -9,6 +10,9 @@ def create_app():
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+
+    #Register the amenities namespace
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     #Register the places namespace
     api.add_namespace(places_ns, path='/api/v1/places')
--- part 2/hbnb/app/services/facade.py
+++ part 2/hbnb/app/services/facade.py
@@ -1,14 +1,15 @@
 """Placeholder for Services facade.py"""
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
         # self.review_repo = InMemoryRepository()
-        # self.amenity_repo = InMemoryRepository()
 
 
     """
@@ -30,6 +31,23 @@ class HBnBFacade:
         self.user_repo.update(user_id, new_data)
         return self.get_user(user_id)
 
+    """
+    AMENITIES
+    """
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.get_amenity(amenity_id)
 
     """
     PLACES
@@ -38,13 +56,14 @@ class HBnBFacade:
         return self.place_repo.get(place_id)
 
     def get_place_all(self):
-        return self.place_repo.get_all
+        return self.place_repo.get_all()
 
     def create_place(self, place_data):
         #create place containing place_data
-        place = Place(**place_data) 
+        place = Place(**place_data)
         self.place_repo.add(place)
         return place
 
     def update_place(self, place_id, place_data):
-        return self.place_repo.update(place_id, place_data)
+        self.place_repo.update(place_id, place_data)
+        return self.get_place(place_id)
